289bbe7698584c84793b7b29b2eac985d8e1a9b4
0HooHI <youngho030411@gmail.com>
2024-12-27 13:21:09 +0900

    18-6

--- 파이썬 과제/test.py
+++ 파이썬 과제/test.py
@@ -1,7 +1,14 @@
-bus_pay = 1350
-balance = int(input())
+start, stop = map(int, input().split())
+ 
+i = start
 
-while(balance > 0):
-    balance -= bus_pay
-    if(balance >= 0):
-        print(balance)
+while True:
+    if(i > stop):
+        break
+    x = str(i);
+    if(x[len(x) - 1] == '3') :
+        i+=1
+        continue;
+    print(i, end=' ')
+    i += 1
+
