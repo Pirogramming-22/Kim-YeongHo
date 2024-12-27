994fac3a4246c73da28335f4f8304736b563c5a8
0HooHI <youngho030411@gmail.com>
2024-12-27 10:11:51 +0900

    8-5

--- 파이썬 과제/test.py
+++ 파이썬 과제/test.py
@@ -1,5 +1,6 @@
-a, b = input('숫자 두 개를 입력하세요: ').split()    # 입력받은 값을 공백을 기준으로 분리
-a = int(a)    # 변수를 정수로 변환한 뒤 다시 저장
-b = int(b)    # 변수를 정수로 변환한 뒤 다시 저장
- 
-print(a + b)
+kor,eng,mat,sci = map(int , input().split())
+
+if(kor>=90 and eng >80 and mat >85 and sci>=80) :
+    print(True)
+else :
+    print(False)
