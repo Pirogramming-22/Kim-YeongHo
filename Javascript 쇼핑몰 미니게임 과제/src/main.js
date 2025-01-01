253ee2f9f05395ee6d043d01793e9c0b0679737f
0HooHI <youngho030411@gmail.com>
2025-01-01 14:01:10 +0900

    쇼핑몰 미니게임 1-5

--- Javascript 쇼핑몰 미니게임 과제/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -5,11 +5,21 @@ function loadItems(){
         .then(json => json.items);
 }
 
+//Update List with the given items
+function displayItems(items){
+    const container = document.querySelector('.items');
+    container.innerHTML = items.map(item => createHTMLstring(item)).join('');
+}
 
-
-
-
-
+//create HTML list items from the given data item
+function createHTMLstring(item){
+    return `
+      <li class="item">
+        <img src="${item.image}" alt="${item.type}" class="item__thumbnail">
+        <span class="item__description">${item.gender},${item.size}</span>
+      </li>
+    `;
+}
 
 
 
@@ -17,7 +27,7 @@ function loadItems(){
 loadItems()
     .then(items => {
         console.log(items);
-/*         displayItems(items);
-        setEventListeners(items); */
+        displayItems(items);
+        // setEventListeners(items);
     })
     .catch(console.log);
