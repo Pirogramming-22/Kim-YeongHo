562a56ac866aa585890c0b285df6ea93c0630235
0HooHI <youngho030411@gmail.com>
2025-01-02 21:07:12 +0900

    파이썬과제 44-6

--- 파이썬 과제/test.py
+++ 파이썬 과제/test.py
@@ -1,19 +1,3 @@
-# class NotPalindromeError(Exception):
-#     def __init__(self):
-#         super().__init__('회문이 아닙니다.')
-
-def palindrome(word):
-    wordReverse= ''.join(reversed(word))
-    if(word != wordReverse):
-        raise NotPalindromeError()
-    else:
-        print(f"{word}")
-
-
-
-
-try:
-    word = input()
-    palindrome(word)
-except NotPalindromeError as e:
-    print(e)
+from math import pi 
+radius = float(input())
+print(f"{pi*(radius**2)}")
