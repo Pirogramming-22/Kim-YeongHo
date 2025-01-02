ebaa494d8f7c98dde4cb8f7a5ff1596257af93eb
0HooHI <youngho030411@gmail.com>
2025-01-02 20:21:06 +0900

    파이썬과제 35-6

--- 파이썬 과제/test.py
+++ 파이썬 과제/test.py
@@ -1,11 +1,33 @@
-class Calc:
-    @staticmethod
-    def add(a, b):
-        print(a + b)
+class Time:
+    def __init__(self ,hour, minute, second):
+        hour = hour
+        minute = minute
+        second = second
+        
+    @classmethod
+    def is_time_valid(cls ,timeString):
+        Time.hour , Time.minute , Time.second = map(int , timeString.split(":"))
+        if(Time.hour < 0 or Time.hour > 24):
+            return 0
+        elif(Time.minute <0 or Time.minute > 60):
+            return 0
+        elif(Time.second <0 or Time.second > 60):
+            return 0
+        else:
+            return 1
+        
+    @classmethod
+    def from_string(cls, timeString):
+        Time.hour , Time.minute , Time.second = map(int , timeString.split(":"))
+        return cls(cls.hour,Time.minute,Time.second)
+        
+
+
  
-    @staticmethod
-    def mul(a, b):
-        print(a * b)
+time_string = input()
  
-Calc.add(10, 20)    # 클래스에서 바로 메서드 호출
-Calc.mul(10, 20)    # 클래스에서 바로 메서드 호출
+if Time.is_time_valid(time_string):
+    t = Time.from_string(time_string)
+    print(t.hour, t.minute, t.second)
+else:
+    print('잘못된 시간 형식입니다.')
