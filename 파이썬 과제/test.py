3adf0853050faf88f8d3e41d25645c4607d8358b
0HooHI <youngho030411@gmail.com>
2025-01-02 20:38:28 +0900

    파이썬과제 36-9

--- 파이썬 과제/test.py
+++ 파이썬 과제/test.py
@@ -1,33 +1,18 @@
-class Time:
-    def __init__(self ,hour, minute, second):
-        hour = hour
-        minute = minute
-        second = second
-        
-    @classmethod
-    def is_time_valid(cls ,timeString):
-        Time.hour , Time.minute , Time.second = map(int , timeString.split(":"))
-        if(Time.hour < 0 or Time.hour > 24):
-            return 0
-        elif(Time.minute <0 or Time.minute > 60):
-            return 0
-        elif(Time.second <0 or Time.second > 60):
-            return 0
-        else:
-            return 1
-        
-    @classmethod
-    def from_string(cls, timeString):
-        Time.hour , Time.minute , Time.second = map(int , timeString.split(":"))
-        return cls(cls.hour,Time.minute,Time.second)
-        
-
-
+class Animal:
+    def eat(self):
+        print('먹다')
  
-time_string = input()
- 
-if Time.is_time_valid(time_string):
-    t = Time.from_string(time_string)
-    print(t.hour, t.minute, t.second)
-else:
-    print('잘못된 시간 형식입니다.')
+class Wing:
+    def flap(self):
+        print('파닥거리다')
+
+class Bird(Animal , Wing):
+    def fly(self):
+        print("날다")
+
+b = Bird()
+b.eat()
+b.flap()
+b.fly()
+print(issubclass(Bird, Animal))
+print(issubclass(Bird, Wing))
