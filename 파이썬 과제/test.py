4772d2ea23940752cb47c1d0f359185e88745058
0HooHI <youngho030411@gmail.com>
2025-01-02 20:01:31 +0900

    파이썬 35-4

--- 파이썬 과제/test.py
+++ 파이썬 과제/test.py
@@ -1,11 +1,11 @@
-class A:
-    x = 9
-    def __init__(self):
-        self.x = 7
-    def getx(self):
-        return self.x
-AAA = A()
-
-print(f"x = {A.x}")
-print(f"x = {AAA.x}")
-print(f"x = {AAA.getx()}")
+class Calc:
+    @staticmethod
+    def add(a, b):
+        print(a + b)
+ 
+    @staticmethod
+    def mul(a, b):
+        print(a * b)
+ 
+Calc.add(10, 20)    # 클래스에서 바로 메서드 호출
+Calc.mul(10, 20)    # 클래스에서 바로 메서드 호출
