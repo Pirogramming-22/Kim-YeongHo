7de0b1ae2dcc08c89bd95267473576e544b019b9
0HooHI <youngho030411@gmail.com>
2025-01-03 11:53:20 +0900

    3단계

--- python_problem/베스킨라빈스31게임.py
+++ python_problem/베스킨라빈스31게임.py
@@ -1,3 +1,18 @@
+class NotValidNumber(Exception):
+    def __init__(self):
+        super().__init__("1,2,3 중 하나를 입력하세요")
 num = 0
 
-num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
+while(True):
+    try:
+        num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
+        if(num not in [1,2,3]):
+            raise NotValidNumber
+    except ValueError:
+        print("정수를 입력하세요")
+        continue
+    except NotValidNumber as E:
+        print(E)
+        continue
+    else:
+        break
