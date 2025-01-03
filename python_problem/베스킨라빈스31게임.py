72d4b25379a1391ad6cdabb275cfc152462d1742
0HooHI <youngho030411@gmail.com>
2025-01-03 13:05:55 +0900

    5단계

--- python_problem/베스킨라빈스31게임.py
+++ python_problem/베스킨라빈스31게임.py
@@ -1,10 +1,10 @@
+from random import randint
+
+""" 예외처리 """
 class NotValidNumber(Exception):
     def __init__(self):
-        super().__init__("1,2,3 중 하나를 입력하세요")
-num = 0
-baskin = 0
-status=True
-
+        super().__init__("1과 3사이의 정수가 아닙니다.")
+""" num입력 """
 def Input_num(x):
     while(True):
         try:
@@ -19,16 +19,31 @@ def Input_num(x):
             continue
         else :
             return x
+""" num을 받고 그걸 통해 출력 """
 def player_declare(num,baskin,status):
     for i in range(num):
         baskin +=1
         if(status == True):
-            print("player:",baskin)
+            print("PlayerA:",baskin)
         else:
-            print("Computer:",baskin)
+            print("PlayerB:",baskin)
         if(baskin == 31):
             return baskin
     return baskin
 
-num = Input_num(num)
-player_declare(num,baskin,status)
+""" 게임 전체 코드 """
+
+num = 0
+baskin = 0
+status = True
+
+while(baskin <31):
+    if(status == True): #플레이어 1의 차례
+        num = Input_num(num)
+        baskin = player_declare(num,baskin,status)
+        status = not status
+    else: #플레이어 2의 차례
+        num = randint(1,3)
+        baskin = player_declare(num,baskin,status)
+        status = not status
+
