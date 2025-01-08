748dfed6c2891a5940da8547e242c8b890d5428c
0HooHI <youngho030411@gmail.com>
2025-01-08 20:23:44 +0900

    나의 첫 번째 Django프로젝트

--- DjangoGirls/myvenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\pui89\Desktop\Kim-YeongHo\DjangoGirls\myvenv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
